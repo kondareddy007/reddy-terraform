781ee3cfea02e8c997fd548fbbfbb033f65ec9ba
kondareddy1229 <kondareddy.devops@gmail.com>
2024-09-20 18:34:51 +0530

    reddy-terraform

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -1,10 +1,33 @@
 resource "aws_instance" "web" {
   ami = "ami-0b4f379183e5706b9"
   instance_type = "t2.micro"
-
+  vpc_security_group_ids = [aws_security_group.roboshop-all.id]
 
   tags = {
-    Name = "Hello "
+    Name = "Hello terraform"
   }
-  
+}
+
+resource "aws_security_group" "roboshop-all" {  #this is terraform name
+  name = "roboshop-all-aws"
+  description = "Allow TLS inbound traffic"
+  #vpc_id      = aws_vpc.main.id  #if you not privide it will take default VPC
+
+  ingress {
+    description = "Allow all ports"
+    from_port = 0 # 0 means allow all ports
+    to_port = 0
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "roboshop-all-aws"
+  }
 }
