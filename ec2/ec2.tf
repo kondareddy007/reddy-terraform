9815b1047b83dc38bc7867adbc530483053f4fd4
kondareddy1229 <kondareddy.devops@gmail.com>
2024-12-08 12:12:08 +0530

    Jenkins file

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -1,9 +1,9 @@
-resource "aws_instance" "web" {
-  ami = "ami-0b4f379183e5706b9"
-  instance_type = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.roboshop-all.id]
-
-  tags = {
-    Name = "Hello terraform"
-  }
+resource "aws_instance" "web"{
+    
+    for_each = var.instance_name
+    ami = var.ami_id
+    instance_type = each.value
+    tags = {
+      Name = each.key
+    }
 }
--- ec2/variable.tf
+++ ec2/variable.tf
@@ -1,17 +1,10 @@
-variable "sg-name" {
-  type = string
-  default = "roboshop-all-aws"
+variable "ami_id" {
+    type = string
+    default = "ami-0b4f379183e5706b9"
 }
 
-variable "sg-description" {
-  type = string
-  default = "Allow TLS inbound traffic"
-}
+variable "instance_name" {
+    type = list
+    default = [ "jenkins","slave" ]
 
-variable "inbound-from_port" {
-   default = 0
-}
-variable "cidr_blocks" {
-  type = list
-  default = ["0.0.0.0/0"]
 }
